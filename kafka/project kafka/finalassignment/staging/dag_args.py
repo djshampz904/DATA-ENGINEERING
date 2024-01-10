2518466a8c750133d3fd00d00b1a0d7b3a061ee0
djshampz904 <djshampz05@gmail.com>
2024-01-10 18:21:54 +0300

    fixing errros

--- kafka/project kafka/finalassignment/staging/dag_args.py
+++ kafka/project kafka/finalassignment/staging/dag_args.py
@@ -31,21 +31,35 @@ unzip_data = BashOperator(
 # Extract data from csv file from 1st to 4th field
 extract_csv = BashOperator(
     task_id='extract_csv',
-    bash_command=' cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
+    # Rowid, Timestamp, Anonymized Vehicle number, and Vehicle type
+    bash_command='cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
     dag=dag,
 )
 
 # Extract data from tsv file
 extract_tsv = BashOperator(
     task_id='extract_tsv',
-    bash_command="cut -d$'\t' -f5-7 tollplaza-data.tsv > tsv_data.tsv",
+    # Columns Number of axles, Tollplaza id, and Tollplaza code
+    bash_command="cut -d$'\t' -f5-7 tollplaza-data.tsv | tr '\t' ',' > tsv_data.csv",
     dag=dag,
 )
 
 #Extract data from a fixed width file
 extract_fixed_width = BashOperator(
     task_id='extract_fixed_width',
-    bash_command="cut -c48-56 payment-data.txt > fixed_width_data.txt",
+    # Columns Type of Payment code, and Vehicle Code
+    bash_command="cut -c59-69 payment-data.txt | tr ' ' ',' > fixed_width_data.csv",
     dag=dag,
 )
+
+# Consolidate all data into one file
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    # Columns in order of Rowid, Timestamp, Anonymized Vehicle number, Vehicle type, Number of axles, 
+    # Tollplaza id, Tollplaza code, Type of Payment code, and Vehicle Code
+    bash_command="paste -d',' csv_data.csv tsv_data.csv fixed_width_data.csv > consolidated_data.csv",
+)
+
+
+
 
