56e4f5b526b244a2e1f3289b942f731bbde3cae4
djshampz904 <djshampz05@gmail.com>
2024-01-12 12:43:58 +0300

    Refactor data extraction and transformation process

--- kafka/project kafka/finalassignment/staging/dag_args.py
+++ kafka/project kafka/finalassignment/staging/dag_args.py
@@ -40,7 +40,7 @@ extract_csv = BashOperator(
 extract_tsv = BashOperator(
     task_id='extract_tsv',
     # Columns Number of axles, Tollplaza id, and Tollplaza code
-    bash_command="cut -d$'\t' -f5-7 tollplaza-data.tsv | tr '\t' ',' > tsv_data.csv",
+    bash_command="cat tollplaza-data.tsv | tr '\t' ',' | cut -d',' -f5-7 | tr -s ' ' > tsv_data.csv",
     dag=dag,
 )
 
@@ -48,7 +48,7 @@ extract_tsv = BashOperator(
 extract_fixed_width = BashOperator(
     task_id='extract_fixed_width',
     # Columns Type of Payment code, and Vehicle Code
-    bash_command="cut -c59-69 payment-data.txt | tr ' ' ',' > fixed_width_data.csv",
+    bash_command='awk \'{print substr($0, 59, 3) "," substr($0, 63)}\' payment-data.txt | tr -s \' \'  > fixed_width_data.csv',
     dag=dag,
 )
 
@@ -57,9 +57,18 @@ consolidate_data = BashOperator(
     task_id='consolidate_data',
     # Columns in order of Rowid, Timestamp, Anonymized Vehicle number, Vehicle type, Number of axles, 
     # Tollplaza id, Tollplaza code, Type of Payment code, and Vehicle Code
-    bash_command="paste -d',' csv_data.csv tsv_data.csv fixed_width_data.csv > consolidated_data.csv",
+    bash_command="paste  csv_data.csv tsv_data.csv fixed_width_data.csv > consolidated_data.csv",
+    dag=dag,
 )
 
 
+# Transform data This task should transform the vehicle_type field in extracted_data.csv into capital letters and save it into a file named transformed_data.csv in the staging directory.
+transform_data = BashOperator(
+    task_id='transform_data',
+    # Change file data to uppercase 
+    bash_command="cut -d',' -f4 | tr '[:lower:]' '[:upper:]' < consolidate_data.csv > transformed_data.csv",
+    dag=dag,
+)
 
-
+# Define pipeline
+unzip_data >> extract_csv >> extract_tsv >> extract_fixed_width >> consolidate_data >> transform_data
